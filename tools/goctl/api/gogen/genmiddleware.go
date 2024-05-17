8392615c170f5bd45281e724acf91b1809d3861e
lucas <huyanyuan@163.com>
2024-05-17 17:42:37 +0800

    鉴权中间件生成

--- tools/goctl/api/gogen/genmiddleware.go
+++ tools/goctl/api/gogen/genmiddleware.go
@@ -15,29 +15,36 @@ var middlewareImplementCode string
 func genMiddleware(dir string, cfg *config.Config, api *spec.ApiSpec) error {
 	middlewares := getMiddleware(api)
 	for _, item := range middlewares {
-		middlewareFilename := strings.TrimSuffix(strings.ToLower(item), "middleware") + "_middleware"
-		filename, err := format.FileNamingFormat(cfg.NamingFormat, middlewareFilename)
-		if err != nil {
-			return err
-		}
-
-		name := strings.TrimSuffix(item, "Middleware") + "Middleware"
-		err = genFile(fileGenConfig{
-			dir:             dir,
-			subdir:          middlewareDir,
-			filename:        filename + ".go",
-			templateName:    "contextTemplate",
-			category:        category,
-			templateFile:    middlewareImplementCodeFile,
-			builtinTemplate: middlewareImplementCode,
-			data: map[string]string{
-				"name": strings.Title(name),
-			},
-		})
-		if err != nil {
+		if err := genOneMiddleware(dir, cfg, item); err != nil {
 			return err
 		}
 	}
 
+	return nil
+}
+
+func genOneMiddleware(dir string, cfg *config.Config, item string) error {
+	middlewareFilename := strings.TrimSuffix(strings.ToLower(item), "middleware") + "_middleware"
+	filename, err := format.FileNamingFormat(cfg.NamingFormat, middlewareFilename)
+	if err != nil {
+		return err
+	}
+
+	name := strings.TrimSuffix(item, "Middleware") + "Middleware"
+	err = genFile(fileGenConfig{
+		dir:             dir,
+		subdir:          middlewareDir,
+		filename:        filename + ".go",
+		templateName:    "contextTemplate",
+		category:        category,
+		templateFile:    middlewareImplementCodeFile,
+		builtinTemplate: middlewareImplementCode,
+		data: map[string]string{
+			"name": strings.Title(name),
+		},
+	})
+	if err != nil {
+		return err
+	}
 	return nil
 }
